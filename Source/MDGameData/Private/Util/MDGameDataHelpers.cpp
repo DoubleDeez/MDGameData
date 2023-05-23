453ddd7fb740f8458dc47e1e95e6ede107446b53
Dylan Dumesnil <dadumesnil@gmail.com>
2023-05-23 12:03:15 -0700

    Add helpers for setting/getting/binding game data entries on actors

---
+++ Source/MDGameData/Private/Util/MDGameDataHelpers.cpp
@@ -0,0 +1,21 @@
+﻿#include "Util/MDGameDataHelpers.h"
+
+FDelegateHandle MDGameDataHelpers::BindOnActorEntryChangedDelegate(AActor* Actor, const FGameplayTag& DataKey, FSimpleDelegate&& Delegate)
+{
+	UMDGameDataContainer* Container = UMDGameDataBlueprintFunctionLibrary::FindOrCreateGameDataContainerForActor(Actor);
+	if (IsValid(Container))
+	{
+		return Container->BindOnEntryChangedDelegate(DataKey, MoveTemp(Delegate));
+	}
+
+	return {};
+}
+
+void MDGameDataHelpers::UnbindOnActorEntryChangedDelegate(const AActor* Actor, const FGameplayTag& DataKey, const FDelegateHandle& DelegateHandle)
+{
+	UMDGameDataContainer* Container = UMDGameDataBlueprintFunctionLibrary::FindGameDataContainerForActor(Actor);
+	if (IsValid(Container))
+	{
+		return Container->UnbindOnEntryChangedDelegate(DataKey, DelegateHandle);
+	}
+}
---
+++ Source/MDGameData/Public/Util/MDGameDataHelpers.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "MDGameDataBlueprintFunctionLibrary.h"
+#include "MDGameDataContainer.h"
+
+struct FGameplayTag;
+
+/**
+ * Exposed helper functions for using Game Data Containers
+ */
+namespace MDGameDataHelpers
+{
+	MDGAMEDATA_API FDelegateHandle BindOnActorEntryChangedDelegate(AActor* Actor, const FGameplayTag& DataKey, FSimpleDelegate&& Delegate);
+	MDGAMEDATA_API void UnbindOnActorEntryChangedDelegate(const AActor* Actor, const FGameplayTag& DataKey, const FDelegateHandle& DelegateHandle);
+
+	template<typename T>
+	FDelegateHandle BindOnActorEntryChangedDelegate(AActor* Actor, const FGameplayTag& DataKey, TDelegate<void(T)>&& Delegate)
+	{
+		UMDGameDataContainer* Container = UMDGameDataBlueprintFunctionLibrary::FindOrCreateGameDataContainerForActor(Actor);
+		if (IsValid(Container))
+		{
+			return Container->BindOnEntryChangedDelegate(DataKey, MoveTemp(Delegate));
+		}
+
+		return {};
+	}
+	
+	template<typename T>
+	static bool GetGameDataForActor(const AActor* Actor, const FGameplayTag& DataKey, T& OutValue)
+	{
+		UMDGameDataContainer* Container = UMDGameDataBlueprintFunctionLibrary::FindGameDataContainerForActor(Actor);
+		if (IsValid(Container))
+		{
+			const EMDGameDataContainerResult Result = Container->GetData(DataKey, OutValue);
+			return Result == EMDGameDataContainerResult::Success_ExistingEntry;
+		}
+		
+		return false;
+	}
+
+	template<typename T>
+	static bool SetGameDataForActor(AActor* Actor, const FGameplayTag& DataKey, const T& Value)
+	{
+		UMDGameDataContainer* Container = UMDGameDataBlueprintFunctionLibrary::FindOrCreateGameDataContainerForActor(Actor);
+		if (IsValid(Container))
+		{
+			const EMDGameDataContainerResult Result = Container->SetData(DataKey, Value);
+			return Result == EMDGameDataContainerResult::Success_ExistingEntry
+				|| Result == EMDGameDataContainerResult::Success_NewEntry;
+		}
+		
+		return false;
+	}
+};
